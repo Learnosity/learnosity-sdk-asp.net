69123b68247c3b5bf78df4825612f250815e16db
karoltarasiuk <karol.tarasiuk@learnosity.com>
2015-11-04 10:55:08 +1100

    Example project added on a branch.
    Please never merge it to master. It should stay on a branch. It's enough
    to rebase this branch every time you want to test something.

---
+++ LearnositySDKExample/Controllers/JSController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnositySDKExample.Controllers
+{
+    public class JSController : Controller
+    {
+        public ActionResult Assess()
+        {
+            ViewBag.heading = "Assess API";
+            ViewBag.JSON = LearnositySDK.Examples.Assess.Simple();
+            return View();
+        }
+
+        public ActionResult Items()
+        {
+            ViewBag.heading = "Items API";
+            ViewBag.description = "<p>Prepare your security, assessment configuration, item and question information and we'll do the rest.</p>";
+            ViewBag.JSON = LearnositySDK.Examples.Items.Simple();
+            return View();
+        }
+
+        public ActionResult Author(string mode = "item_edit")
+        {
+            ViewBag.heading = "Author API";
+            ViewBag.description = (mode == "item_edit")
+                                ? "<p>Allow you to create/edit your own content which are pulled through Learnosity ItemBank in your own authoring environment.</p>"
+                                : "<p>Retrieve content from the Learnosity ItemBank to embed in your own authoring environment.</p>";
+            ViewBag.JSON = LearnositySDK.Examples.Author.Simple(mode);
+            return View();
+        }
+
+        public ActionResult Questions()
+        {
+            ViewBag.heading = "Questions API";
+            ViewBag.description = "<p>Prepare your question JSON and security credentials, we'll do the rest!</p>";
+            string uuid = "";
+            ViewBag.JSON = LearnositySDK.Examples.Questions.Simple(out uuid);
+            ViewBag.UUID = uuid;
+            return View();
+        }
+
+        public ActionResult Reports()
+        {
+            ViewBag.heading = "Reports API";
+            ViewBag.description = "<p>Prepare your report configuration and security credentials, we'll do the rest!</p>";
+            ViewBag.JSON = LearnositySDK.Examples.Reports.Simple();
+            return View();
+        }
+    }
+}
---
+++ LearnositySDKExample/Controllers/RemotesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnositySDKExample.Controllers
+{
+    public class RemotesController : Controller
+    {
+        public ActionResult Data()
+        {
+            string JSON;
+            ViewBag.heading = "Data API";
+            ViewBag.description = "<p>Use the Data API to retrieve or update content in the Learnosity Assessment Platform.</p>";
+            ViewBag.RESULT = LearnositySDK.Examples.Data.Simple(out JSON);
+            ViewBag.JSON = JSON;
+            return View();
+        }
+
+        public ActionResult DataApi()
+        {
+            ViewBag.heading = "Data API";
+            ViewBag.description = "<p>Use the Data API to retrieve or update content in the Learnosity Assessment Platform.</p>";
+            ViewBag.RESULT = LearnositySDK.Examples.Data.DataApi();
+            return View();
+        }
+
+        public ActionResult DataApiRecursive()
+        {
+            ViewBag.heading = "Data API";
+            ViewBag.description = "<p>Use the Data API to retrieve or update content in the Learnosity Assessment Platform.</p>";
+            ViewBag.RESULT = LearnositySDK.Examples.Data.DataApiRecursive();
+            return View();
+        }
+
+        public ActionResult Schemas()
+        {
+            string URL;
+            ViewBag.heading = "Schemas API";
+            ViewBag.description = "<p>Retrieve JSON schema information for question types, attributes and templates.</p>";
+            ViewBag.RESULT = LearnositySDK.Examples.Schemas.Simple(out URL);
+            ViewBag.URL = URL;
+            return View();
+        }
+    }
+}

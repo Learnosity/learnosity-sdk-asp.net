79668457f042ab5e751481d103b6ae990751d4d3
gonzalozawa <gonzalo.zawadzki@learnosity.com>
2018-08-20 17:37:41 +0100

    [FEATURE] Add Integration tests project

---
+++ LearnositySDKIntegrationTests/LearnositySDKIntegrationTests.cs
@@ -0,0 +1,73 @@
+using System;
+using Xunit;
+using LearnositySDK.Utils;
+using LearnositySDK.Request;
+
+namespace LearnositySDKIntegrationTests
+{
+    public class LearnositySDKIntegrationTests
+    {
+        private readonly string consumerKey = Config.ConsumerKey;
+        private readonly string consumerSecret = Config.ConsumerSecret;
+        private readonly string domain = Config.Domain;
+        private readonly string baseDataAPIUrl = buildDataAPIBaseUrl(Config.ENV, Config.REGION, Config.VER_DataAPI);
+
+        [Fact]
+        public void DataAPIGetItems()
+        {
+            // Arrange
+            JsonObject JsonResponse;
+
+            string endpoint = this.baseDataAPIUrl + "/itembank/items";
+
+            string action = "get";
+
+            JsonObject security = new JsonObject();
+            security.set("consumer_key", this.consumerKey);
+            security.set("domain", this.domain);
+
+            JsonObject request = new JsonObject();
+            request.set("limit", 3);
+
+            // Act
+            DataApi da = new DataApi();
+            Remote r = da.request(endpoint, security, this.consumerSecret, request, action);
+
+            JsonResponse = JsonObjectFactory.fromString(r.getBody());
+
+            // Assert
+            Assert.Equal(
+                JsonResponse.getJsonObject("meta").getBool("status"),
+                true
+            );
+        }
+
+        private static string buildDataAPIBaseUrl(string env, string region, string version)
+        {
+            string regionDomain = "";
+            string envDomain = "";
+            string versionPath = "v1";
+
+            if (!string.IsNullOrEmpty(region))
+            {
+                regionDomain = string.Concat("-", region);
+            }
+
+            if (!string.IsNullOrEmpty(env) && env.CompareTo("prod") != 0)
+            {
+                envDomain = "." + env;
+            }
+
+            if (!string.IsNullOrEmpty(env) && env.CompareTo("vg") == 0)
+            {
+                versionPath = "latest";
+            }
+            else if (!string.IsNullOrEmpty(version))
+            {
+                versionPath = version;
+            }
+
+            return "https://data" + regionDomain + envDomain + ".learnosity.com/" + versionPath;
+        }
+    }
+}
